21aa8ad4c2706652b4e2b405ad1907568cecb8cc
bbogdanffy2 <b.bogdanffy@dxc.com>
2022-02-25 12:26:55 +0100

    refactoring and optimization

--- JSON_key_util.js
+++ JSON_key_util.js
@@ -20,51 +20,29 @@
  *
  * @return {Array} Returns an array with all the key-s found in the given JSON object.
  */
-function getAllJSONKeys(json, structure, allKeys) {
-    let keys = Object.keys(json);
-  
-    if(allKeys) {
-        console.log("Step in inner JSON object:");
-        console.log(json);
-    } else {
-        console.log("Start iterating through JSON object...");
-        console.log(json);
-        allKeys = [];
-    }
+const getAllJSONKeys = (json, structure = null, allKeys = []) => {
+    const keys = Object.keys(json);
 
     if(keys) {
-        let originalStructure = structure;
+        const originalStructure = structure;
         keys.forEach(key => {
             structure = originalStructure;
-
-            if(structure === null || structure === undefined) {
-                structure = key;
-            } else {
-                structure += `.${key}`;
-            }
-
+            structure ? structure += `.${key}` : structure = key;
             allKeys.push(structure);
             
             // Check if json[key] is not null -> null is an object in JS
             if(json[key] && typeof json[key] === "object") {
                 let keysOfChild = Object.keys(json[key]);
+                let childPath;
+                let grandChild;
 
                 keysOfChild.forEach(innerKey => {
-                    let childPath = "";
-
-                    if(structure === null || structure === undefined) {
-                        childPath = `${key}.${innerKey}`;  
-                    } else {
-                        childPath = `${structure}.${innerKey}`;
-                    }
-
+                    childPath = `${structure}.${innerKey}`;
                     allKeys.push(childPath);
 
-                    let grandChild = json[key][innerKey];
-                    if (grandChild) {
-                        if(typeof grandChild === "object") {
-                            getAllJSONKeys(grandChild, childPath, allKeys);
-                        }
+                    grandChild = json[key][innerKey];
+                    if (grandChild && typeof grandChild === "object") {
+                        getAllJSONKeys(grandChild, childPath, allKeys);
                     }
                 });
             }
@@ -99,12 +77,9 @@ function getAllJSONKeys(json, structure, allKeys) {
  *
  * @return {any} Returns the value by the given path from the given JSON object.
  */
-function getJSONValue(path, json) {
-    if(path.startsWith("$.")) {
-        path.replace("$.", "");
-    }
-
-    let splittedPath = path.split(".");
+const getJSONValue = (path, json) => {
+    if(path.startsWith("$.")) path.replace("$.", "");
+    const splittedPath = path.split(".");
 
     splittedPath.forEach(path => {
         json = json[path];
@@ -127,7 +102,7 @@ function getJSONValue(path, json) {
  * @param {Object}  json        The JSON with the key-value pairs.
  *
  */
- function getAllJSONValues(pathArray, json) {
+ const getAllJSONValues = (pathArray, json) => {
     let resultArr = [];
     pathArray.forEach(path => {
         try {
@@ -149,7 +124,7 @@ function getJSONValue(path, json) {
  * @param {Object}  json        The JSON object.
  *
  */
-function printAllJSONValueToConsole(pathArray, json) {
+const printAllJSONValueToConsole = (pathArray, json) => {
     console.log("======================================================================");
     console.log("========= JSON VALUES BY JSON PATH ARRAY:");
     console.log("======================================================================");
@@ -160,3 +135,5 @@ function printAllJSONValueToConsole(pathArray, json) {
     console.log("========= PRINT JSON VALUES END");
     console.log("======================================================================");
 }
+
+export { getAllJSONKeys, getJSONValue, getAllJSONValues, printAllJSONValueToConsole };
